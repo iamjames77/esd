ded906d0bab671d53b7f18167f6b8a8b4d2cf168
iamjames77 <iamjames77@daum.net>
2024-12-01 16:52:11 +0900

    send

--- send.py
+++ send.py
@@ -2,50 +2,34 @@ import RPi.GPIO as GPIO
 import time
 
 # GPIO 핀 설정
-RECEIVER_PIN = 17  # 수신 레이저 센서 핀 번호
-BIT_DURATION = 0.02  # 비트 전송 간격 (송신기와 동일)
-THRESHOLD_DURATION = BIT_DURATION
+LASER_PIN = 18  # 송신 레이저 핀 번호
+BIT_DURATION = 0.02 # 비트 전송 간격 (초)
 
 # GPIO 초기화
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RECEIVER_PIN, GPIO.IN)
+GPIO.setup(LASER_PIN, GPIO.OUT)
 
-def receive_message():
-    binary_message = ''
-    last_time = time.time()
-    connect = False
-    print("Waiting for message...")
-    while True:
-        current_state = 0 if GPIO.input(RECEIVER_PIN) == GPIO.HIGH else 1
-        current_time = time.time()
-        
-        # 신호가 꺼진 경우만 관심
-        if connect:
-            binary_message += '1' if current_state else '0'
-            if(current_time - last_time > 10 * THRESHOLD_DURATION):
-                print(binary_message)
-                break
-            if (current_state == 1):
-                last_time = current_time
-
-        if(current_state and connect == False):
-            print("connected")
-            connect = True
-            last_time = current_time
-        time.sleep(BIT_DURATION)
-
-    # 8비트씩 읽어 아스키 문자로 변환
-    message = ''.join(
-        chr(int(binary_message[i:i+8], 2))
-        for i in range(0, len(binary_message), 8)
-    )
-    return message
+def send_message(message):
+    GPIO.output(LASER_PIN, GPIO.HIGH)
+    time.sleep(BIT_DURATION)
+    for char in message:
+        binary_representation = f'{ord(char):08b}'  # 각 문자를 8비트 이진수로 변환
+        print(binary_representation)
+        for bit in binary_representation:
+            if bit == '1':
+                GPIO.output(LASER_PIN, GPIO.HIGH)  # 레이저 ON
+            else:
+                GPIO.output(LASER_PIN, GPIO.LOW)  # 레이저 OFF
+            time.sleep(BIT_DURATION)
+        # 문자 간 구분을 위한 짧은 레이저 OFF
+        GPIO.output(LASER_PIN, GPIO.LOW)
 
 try:
     while True:
-        received_message = receive_message()
-        print(f"Received message: {received_message}, Time: {time.time()}, Transmission Speed: {BIT_DURATION}")
+        message = input("Enter message to send: ")
+        send_message(message)
+        print(f"Message sent at {time.time()}, TRANSMISSION_SPEED: {BIT_DURATION}")
 except KeyboardInterrupt:
-    print("Receiver stopped.")
+    print("Transmitter stopped.")
 finally:
     GPIO.cleanup()
