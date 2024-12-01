c21e671e2b91135580adf013fc5710496b134015
iamjames77 <iamjames77@daum.net>
2024-12-01 16:53:46 +0900

    receive

--- receive.py
+++ receive.py
@@ -3,8 +3,8 @@ import time
 
 # GPIO 핀 설정
 RECEIVER_PIN = 17  # 수신 레이저 센서 핀 번호
-BIT_DURATION = 0.1  # 비트 전송 간격 (송신기와 동일)
-THRESHOLD_DURATION = BIT_DURATION / 2  # 신호 감지 임계값
+BIT_DURATION = 0.02  # 비트 전송 간격 (송신기와 동일)
+THRESHOLD_DURATION = BIT_DURATION
 
 # GPIO 초기화
 GPIO.setmode(GPIO.BCM)
@@ -13,21 +13,26 @@ GPIO.setup(RECEIVER_PIN, GPIO.IN)
 def receive_message():
     binary_message = ''
     last_time = time.time()
-    
+    connect = False
     print("Waiting for message...")
     while True:
-        current_state = GPIO.input(RECEIVER_PIN)
+        current_state = 0 if GPIO.input(RECEIVER_PIN) == GPIO.HIGH else 1
         current_time = time.time()
         
         # 신호가 꺼진 경우만 관심
-        if not current_state:
-            duration = current_time - last_time
-            if duration >= BIT_DURATION * 10:  # 종료 신호
+        if connect:
+            binary_message += '1' if current_state else '0'
+            if(current_time - last_time > 10 * THRESHOLD_DURATION):
                 print(binary_message)
                 break
-            elif duration >= THRESHOLD_DURATION:
-                binary_message += '1' if duration >= BIT_DURATION else '0'
+            if (current_state == 1):
                 last_time = current_time
+
+        if(current_state and connect == False):
+            print("connected")
+            connect = True
+            last_time = current_time
+        time.sleep(BIT_DURATION)
 
     # 8비트씩 읽어 아스키 문자로 변환
     message = ''.join(
@@ -39,7 +44,7 @@ def receive_message():
 try:
     while True:
         received_message = receive_message()
-        print(f"Received message: {received_message}")
+        print(f"Received message: {received_message}, Time: {time.time()}, Transmission Speed: {BIT_DURATION}")
 except KeyboardInterrupt:
     print("Receiver stopped.")
 finally:
