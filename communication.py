b228b758b636e7e8aad9789eebde5f30b307a5bb
iamjames77 <iamjames77@daum.net>
2024-12-16 19:30:54 +0900

    communication

--- communication.py
+++ communication.py
@@ -1,12 +1,11 @@
 import threading
 import time
-import crc
 import RPi.GPIO as GPIO
 
 # GPIO 핀 설정
 LASER_PIN = 18  # 송신 레이저 핀 번호
 RECEIVER_PIN = 17  # 수신 레이저 센서 핀 번호
-BIT_DURATION = 0.02 # 비트 전송 간격 (초)
+BIT_DURATION = 0.05 # 비트 전송 간격 (초)
 
 # GPIO 초기화
 GPIO.setmode(GPIO.BCM)
@@ -20,6 +19,8 @@ send_list = []
 list_pointer = 0
 
 timeout = 5
+
+error_event = threading.Event()
 
 def compute_CRC(data, polynomial, crc_bits):
     data += '0' * crc_bits
@@ -55,6 +56,10 @@ def send_protocol(protocol):
         send_bit('110110')
     elif protocol == 'RESEND':
         send_bit('111010')
+    elif protocol == 'RESEND REQUEST':
+        send_bit('111101')
+    elif protocol == 'CONNECTION END':
+        send_bit('110011')
 
 def send_msg_size(msg_size):
     msg_size_bit = f'{msg_size:04b}'
@@ -88,15 +93,19 @@ def receive_protocol(timeout):
         if current_state:
             time.sleep(BIT_DURATION)
             received_protocol = receive_bit(5)
-            print(received_protocol)
+            #print(received_protocol)
             if (received_protocol == '11110'):
                 return 'MSG RECEIVE'
             elif (received_protocol == '10111'):
                 return 'HANDSHAKE'
             elif (received_protocol == '10110'):
                 return 'HANDSHAKE REPLY'
-            elif (receive_protocol == '11010'):
+            elif (received_protocol == '11010'):
                 return 'RESEND'
+            elif (received_protocol == '11101'):
+                return 'RESEND REQUEST'
+            elif (received_protocol == '10011'):
+                return 'CONNECTION END'
         time.sleep(BIT_DURATION)
 
         if (time.time() - start_time >= timeout):
@@ -105,11 +114,11 @@ def receive_protocol(timeout):
 def receive_msg_size():
     msg_size_bit = receive_bit(4)
     msg_size = int(msg_size_bit, 2)
-    print(f'Message Size: {msg_size}')
+    #print(f'Message Size: {msg_size}')
     return msg_size
 
 
-def receive_message(msg_size):
+def receive_message(msg_size, msg_index):
     msg_bit = receive_bit(msg_size * 8)
     crc_bit = receive_bit(crc_bits)
     if(compute_CRC(msg_bit, polynomial, crc_bits) == crc_bit):
@@ -118,10 +127,12 @@ def receive_message(msg_size):
             chr(int(msg_bit[i:i+8], 2))
             for i in range(0, len(msg_bit), 8)
         )
+        #print(message)
         return message
     else:
-        send_protocol('RESEND')
-        send_bit(msg_index)
+        send_protocol('RESEND REQUEST')
+        send_msg_index(msg_index)
+        #print('error')
         return '!!ERROR!!'
 
 def sender_thread():
@@ -143,7 +154,7 @@ def sender_thread():
                     print('HANDSHAKE REPLY')
                     received_msg_size = receive_msg_size()
                     msg_index = receive_bit(8)
-                    print(f'{int(msg_index, 2)}')
+                    #print(f'{msg_index}, {received_msg_size}')
                     if(msg_chunk == received_msg_size and int(msg_index,2) == list_pointer):
                         break
                 fail_count += 1
@@ -153,7 +164,9 @@ def sender_thread():
             if (fail_count == 10):
                 print("connection error")
                 continue
+            send_protocol('MSG')
             byte_sent = 0
+            error_event.set()
             while byte_sent < msg_size:
                 msg_chunk_size = 15 if byte_sent + 15 < msg_size else msg_size - byte_sent
                 send_list.append([msg_chunk_size, list_pointer, message[byte_sent:byte_sent + msg_chunk_size]])
@@ -165,6 +178,22 @@ def sender_thread():
     finally:
         GPIO.cleanup()
 
+def error_handle_thread():
+    while True:
+        # Event가 설정될 때만 실행
+        error_event.wait()
+        #fprint("error handle")
+        received_protocol = receive_protocol(float('inf'))
+        #print(received_protocol)
+        if received_protocol == 'RESEND REQUEST':
+
+            send_protocol('RESEND')
+            received_index = int(receive_bit(8), 2)
+            for i in range(received_index, list_pointer):
+                laser(i)
+        # Event가 꺼지면 대기
+        error_event.clear()
+
 def receiver_thread():
     try:
         while True:
@@ -174,21 +203,42 @@ def receiver_thread():
                     received_index = receive_bit(8)
                     send_protocol('HANDSHAKE REPLY')
                     send_msg_size(received_chunk)
+                    send_bit(received_index)
+                    #print(received_index)
                 elif received_protocol == 'RESEND':
-                    recived_chunk = receive_bit(8)
+                    received_chunk = receive_bit(8)
                     laser(received_chunk)
             if(received_protocol == 'MSG RECEIVE'):
                 received_message = ''
                 received_message = [0] * received_chunk
+                received_index = int(received_index, 2)
                 msg_size = receive_msg_size()
                 while msg_size !=0 :
                     chunk_index = receive_bit(8)
-                    received_message[chunk_index - received_index] = receive_message(msg_size)    
+                    chunk_index = int(chunk_index, 2)
+                    #print(chunk_index, received_index) 
+                    message = receive_message(msg_size, chunk_index)
+                    if(chunk_index - received_index > received_chunk):
+                        send_protocol('RESEND REQUEST')
+                        send_msg_index(safe_chunk)
+                        message = '!!ERROR!!'
+                    if(message == '!!ERROR!!'):
+                        print(message)
+                        while True:
+                            received_protocol = receive_protocol(float('inf'))
+                            if (received_protocol == 'RESEND'):
+                                break
+                    else:
+                        received_message[chunk_index - received_index] = message
+                        safe_chunk = chunk_index
+                    msg_size =receive_msg_size()
+                send_protocol('CONNECTION END')
                 print(f"Received message: {received_message}, Time: {time.time()}, Transmission Speed: {BIT_DURATION}")
     except KeyboardInterrupt:
         print("Receiver stopped.")
     finally:
         GPIO.cleanup()
+
 
 def laser(index):
     top_send_element = send_list[index]
@@ -205,10 +255,12 @@ if __name__ == "__main__":
         # 송신 및 수신을 위한 쓰레드 생성
         sender = threading.Thread(target=sender_thread, daemon=True)
         receiver = threading.Thread(target=receiver_thread, daemon=True)
+        error_handler = threading.Thread(target=error_handle_thread, daemon=True)
 
         # 쓰레드 시작
         sender.start()
         receiver.start()
+        error_handler.start()
         
         # 메인 쓰레드 대기
         sender.join()
